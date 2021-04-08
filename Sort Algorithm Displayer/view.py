3cf4beb771f585c118ccc7c88366454a6b47baff
adrisanpu <71441992+adrisanpu@users.noreply.github.com>
2021-04-08 09:52:26 +0200

    Add files via upload

---
+++ Sort Algorithm Displayer/view.py
@@ -0,0 +1,37 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+from gi.repository import Gdk
+
+class myWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self,title="SORT ALGORITHM DISPLAYER")
+        
+        hbox = Gtk.Box(spacing=20)
+        hbox.set_homogeneous(False)
+        
+        title = Gtk.Label(label="SORT ALGORITHM DISPLAYER")
+        hbox.pack_start(title, True, True, 0)
+        
+        sel_sort = Gtk.Button.new_with_label("SELECTION SORT")
+        sel_sort.connect("clicked", self.on_sel_sort_clicked)
+        hbox.pack_start(sel_sort, True, True, 0)
+        
+        bub_sort = Gtk.Button.new_with_label("SELECTION SORT")
+        bub_sort.connect("clicked", self.on_bub_sort_clicked)
+        hbox.pack_start(bub_sort, True, True, 0)
+        
+        self.add(hbox)
+        
+    def on_sel_sort_clicked(self, sel_sort):
+        print('"selection sort" button was clicked')
+    def on_bub_sort_clicked(self, bub_sort):
+        print('"bubble sort" button was clicked')
+        
+
+
+if __name__ == "__main__":
+    window = myWindow()
+    window.connect("destroy", Gtk.main_quit)
+    window.show_all()
+    Gtk.main()
