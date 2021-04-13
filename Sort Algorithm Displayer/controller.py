5db1986c05918a987d8645471fdc6d7f6a42a344
adrisanpu <71441992+adrisanpu@users.noreply.github.com>
2021-04-13 11:29:32 +0200

    Add files via upload

--- Sort Algorithm Displayer/controller.py
+++ Sort Algorithm Displayer/controller.py
@@ -1,12 +1,13 @@
+from model import *
+from const import *
 
-class controller
-	def __init__(self, window):
+class controller:
+    def __init__(self, window):
         self.view = window
 
     def sort_array(self, input_array, algo):
-    	array = array_to_sort(input_array)
-        if(algo == const.SELECTION_SORT):
-        	array.selection_sort()
-
+        array = array_to_sort(input_array)
+        if(algo == SELECTION_SORT):
+            array.selection_sort()
 
         return array
